239b20f56a2a2ed451a68c25d6f559a61ee12bb8
Abraham Hernandez <abrhernandez@192.168.1.6>
2025-05-07 21:32:38 -0600

    initial

---
+++ src/main/java/com/abrhernandez/meeting_golden/MeetingGoldenApplication.java
@@ -0,0 +1,13 @@
+package com.abrhernandez.meeting_golden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingGoldenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingGoldenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abrhernandez/meeting_golden/MeetingGoldenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrhernandez.meeting_golden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingGoldenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
